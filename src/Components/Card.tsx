4a8664116a0fd38805f028a3457fddc683048263
Gaël Bouquain <gael.bouquain@gmail.com>
2023-03-28 21:54:51 +0200

    Change UI
    - Add new fonts
    - Add new button component

--- src/Components/Card.tsx
+++ src/Components/Card.tsx
@@ -4,7 +4,15 @@ import sass from "./Card.module.sass"
 type Props = {
   children?: JSXElement[] | JSXElement,
   className?: string,
-  style?: string
+  style?: string,
+  shineColor?: string
+}
+
+type ButtonProps = {
+  children?: JSXElement,
+  className?: string,
+  style?: string,
+  shineColor?: string
 }
 
 export const Card = (props: Props) => {
@@ -35,7 +43,7 @@ export const Card = (props: Props) => {
 
   let handleLeave = () => {
     if (!card) return
-    card.style.transform = `perspective(700px) rotateX(0deg) rotateY(0deg)`
+    card.style.transform = `perspective(600px) rotateX(0deg) rotateY(0deg)`
   }
 
   onMount(() => {
@@ -50,13 +58,51 @@ export const Card = (props: Props) => {
 
   return (
     <div
-      style={props.style || ""}
+      style={`${props.shineColor ? "--shine-color: " + props.shineColor : ""}; ${props.style || ""}`}
       onPointerMove={handleMove}
       onPointerLeave={handleLeave}
-      class={sass.card}
+      class={`${sass.card} ${props.className || ""}`}
       ref={card}>
       <span class={sass.shine} ref={shine} />
       {props.children}
+    </div>
+  )
+}
+
+export const ButtonCard = (props: ButtonProps) => {
+  // Refs
+  let card: HTMLElement | undefined = undefined
+
+  // Variables
+  let [cardX, setCardX] = createSignal(0)
+  let [cardY, setCardY] = createSignal(0)
+  let [cardWidth, setCardWidth] = createSignal(0)
+  let [cardHeight, setCardHeight] = createSignal(0)
+
+  // Logic
+  let handleMove = (evt: PointerEvent) => {
+    if (!card) return
+
+    let x = evt.pageX - cardX()
+    let y = evt.pageY - cardY()
+    card.style.background = `radial-gradient(120% calc(120% * 21/13) at ${x / cardWidth() * 100}% ${y / cardHeight() * 100}%, var(--shine-color, #5ebfff) 0%, #00000000 85%), #000000`
+  }
+
+  onMount(() => {
+    let cardRect = card!.getBoundingClientRect()
+    setCardX(cardRect.x)
+    setCardY(cardRect.y)
+    setCardWidth(cardRect.width)
+    setCardHeight(cardRect.height)
+  })
+
+  return (
+    <div
+      style={`${props.shineColor ? "--shine-color: " + props.shineColor : ""}; ${props.style || ""}`}
+      onPointerMove={handleMove}
+      class={`${sass.button} ${props.className || ""}`}
+      ref={card}>
+      <span class={sass.text}>{props.children}</span>
     </div>
   )
 }
--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,7 +1,5 @@
 import behideLogo from "../assets/behide.svg"
-import githubLogo from "../assets/github.svg"
-import arrowDown from "../assets/arrow-down.svg"
-import * as Button from "../Components/Button"
+import { ButtonCard } from "../Components/Card"
 import sass from "./Home.module.sass"
 
 const Banner = () => (
@@ -14,36 +12,23 @@ const Banner = () => (
   </div>
 )
 
-const Button_ = ({ icon, iconAlt, cssClass, text, onClick }: { icon: string, iconAlt: string, cssClass: string, text: string, onClick: () => void }) => (
-  <button class={cssClass} onClick={onClick}>
-    <img src={icon} alt={iconAlt} />
-    <span class={sass.separator} />
-    <span class={sass.text}>{text}</span>
-  </button>
-)
-
 export default () => {
-  const navigateToRepo = () => document.location.assign("https://github.com/behide-game/")
-
   return (
     <div class={sass.home}>
-      <div class={sass["left-part"]}>
-        <Banner />
-      </div>
-      <div class={sass["right-part"]}>
+      <Banner />
+
+      <div id={sass.ball}>
+      <div class={sass.content}>
+        <h1>What is that !?</h1>
+
         <div class={sass.features}>
-          <div class={sass.feature}><span>What is that ?</span></div>
-          <span class={sass.feature}>Free to play</span>
-          <span class={sass["complex-feature"]}>
-            <span class={sass.title}>Open source</span>
-            <span class={sass.action} style={`--background-url: url(${githubLogo})`} onClick={navigateToRepo} />
-          </span>
-          <span class={sass.feature}>Every man for himself</span>
+          <ButtonCard shineColor="#00ff165f">Open source</ButtonCard>
+          <ButtonCard shineColor="#009aff5f">Free{"\n"}to play</ButtonCard>
+          <ButtonCard shineColor="#ff002e5f">Made with{"\n"}Unity</ButtonCard>
         </div>
 
-        <div class={sass.arrow} style={`--arrow-down-path: url("${arrowDown}")`} />
-
-        <Button.Primary text="Play" onClick={navigateToRepo} />
+        <a href="https://github.com/behide-game/">See on GitHub</a>
+      </div>
       </div>
     </div>
   )
